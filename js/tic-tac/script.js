e594b13dab73f65a12b9c7979d32f5b7be2a906f
Relax <Loginovskikh.K@gmail.com>
2018-10-21 21:38:05 +0300

    Еhe processing of messages for a log

--- js/tic-tac/script.js
+++ js/tic-tac/script.js
@@ -7,8 +7,10 @@ const LOG      = document.getElementById("log");
 // In theis block, only the attachment of different handlers will be executed
 initField(SIDE, FIELD_ID);
 log("message 1");
-log("message 2");
+log("message 2", "warning");
 log("message 3");
+log("message 4", "warning");
+log("message 5", "error");
 
 // function that creates a playing field
 function initField(side, id){
@@ -17,8 +19,9 @@ function initField(side, id){
 	document.getElementById(id).innerHTML = field;
 }
 
-function log(str) {
-	let res = "<div class='message'>" + str + "</div>";
+function log(str, status) {
+	let stat = status || "success";
+	let res = "<div class='message " + stat + "'>" + str + "</div>";
 	LOG.innerHTML = res + LOG.innerHTML;
 }
 
