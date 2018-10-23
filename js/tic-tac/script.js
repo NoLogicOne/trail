62ba5307953919f90c21f7a15ab87f3fd25579cf
Relax <Loginovskikh.K@gmail.com>
2018-10-23 17:20:22 +0300

    correct work in form with setup

--- js/tic-tac/script.js
+++ js/tic-tac/script.js
@@ -1,16 +1,28 @@
-
-const SIDE     = 0;
+// It's a block 0f constants
 const TABLE    = document.getElementById("table");
 const REFRESH  = document.getElementById("refresh");
 const LOG      = document.getElementById("log");
 
-initField(SIDE);
+// These are custom variables
+var player1 = "Player1";
+var player2 = "Player2";
+var side    = 0;
+var winLine = 0;
+
+// These are game's variables
+var currentClass  = "cross";
+var currentPlayer = player1;
+
 // In theis block, only the attachment of different handlers will be executed
 TABLE.addEventListener("click", tableHandler());
 document.getElementById("clear_log").addEventListener("click", clearLog);
+document.getElementById("start-game").addEventListener("click", startHandler);
+// document.getElementById("setup-form").addEventListener("submit", startHandler);
+document.getElementById("setup-form").addEventListener("submit", log("submit"));
+document.getElementById("setup-form").onsubmit = startHandler;
 
 // function that creates a playing field
-function initField(side){
+function initGame(side){
 	let field = "<td></td>".repeat(side);
 	field = ("<tr>" + field + "</tr>").repeat(side);
 	TABLE.innerHTML = field;
@@ -38,6 +50,16 @@ function tableHandler(){
 			toggleCurrent();
 		}
 	}
+}
+
+function startHandler(event){
+	event.preventDefault();
+	log("startHandler");
+	player1 = document.getElementById("player1").value;
+	player2 = document.getElementById("player2").value;
+	side    = document.getElementById("side").value;
+	winLine = document.getElementById("line-for-win").value;
+	initGame(side);
 }
 
 function clearLog(){
